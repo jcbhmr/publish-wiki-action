2a096f14af3af62c8faa87a7831f78fea344bb86
Jacob Hummer <jcbhmr@outlook.com>
2023-03-12 21:36:51 -0500

    Add beginner idea of git subtree

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,16 +8,6 @@ if [ -z "$GH_TOKEN" ]; then
   exit 1
 fi
 
-if [ -z "$GH_MAIL" ]; then
-  echo "GH_MAIL ENV is missing. Use the email for a user that can push to this repository."
-  exit 1
-fi
-
-if [ -z "$GH_NAME" ]; then
-  echo "GH_NAME ENV is missing. Use the username for a user that can push to this repository."
-  exit 1
-fi
-
 if [ -z "$REPO" ]; then
   echo "REPO ENV is missing. Using the current one."
   REPO=$GITHUB_REPOSITORY
@@ -27,6 +17,8 @@ if [ -z "$WIKI_DIR" ]; then
     echo "WIKI_FOLDER ENV is missing, using default wiki/"
     WIKI_DIR='wiki/'
 fi
+
+if [ -n "$GH_NAME" -a -n "$GH_MAIL" ]; then
 
 # Disable Safe Repository checks
 git config --global --add safe.directory "/github/workspace"
@@ -69,3 +61,25 @@ git config user.email $GH_MAIL
 git add .
 git commit -m "$message"
 git push origin master
+
+else
+  # FUTURE: Rename "WIKI_DIR" option to "path" in action.yml
+  INPUT_PATH="$WIKI_DIR"
+
+  echo "🟪 Splitting subtree on $INPUT_PATH..."
+  # Git will extract the specified subdirectory and create a new commit history
+  # that includes only the files and directories within that subdirectory. This
+  # new commit history will be stored in a new Git branch, which will be created
+  # automatically by the subtree command. The new branch will include all of the
+  # history for the original repository that relates to the specified
+  # subdirectory. It then prints out the ref to that branch which we capture in
+  # a variable.
+  ref=$(git subtree split -P "$INPUT_PATH")
+
+  # TODO: Include $GITHUB_TOKEN in here to make it work pushing to other repos
+  # outside of the current $GITHUB_REPOSITORY
+  remote="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY.wiki.git"
+  echo "🟪 Pushing $ref too $remote..."
+  git push -f "$remote" "$ref:master"
+  echo "🟩 Successfully published to the GitHub wiki for $GITHUB_REPOSITORY."
+fi
